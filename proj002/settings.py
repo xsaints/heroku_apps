f50b70fda40104340ef0e026ee1cb29a849eea10
xsaints <xsaints@yahoo.com>
2017-05-30 19:41:35 -0600

    Added custom 404 and 500 error pages.

--- proj002/settings.py
+++ proj002/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'proj002.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'proj002/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
